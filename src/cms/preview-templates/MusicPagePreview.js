192228aa2081b452e5fce2aec496e80c56255468
Bailey Kane <bailey.orion.kane@gmail.com>
2021-10-31 18:12:27 +0800

    Victim of copy paste

--- src/cms/preview-templates/MusicPagePreview.js
+++ src/cms/preview-templates/MusicPagePreview.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/about-page'
+import { MusicPageTemplate } from '../../templates/music-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
+const MusicPagePreview = ({ entry, widgetFor }) => (
+  <MusicPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
 )
 
-AboutPagePreview.propTypes = {
+MusicPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
 }
 
-export default AboutPagePreview
+export default MusicPagePreview
